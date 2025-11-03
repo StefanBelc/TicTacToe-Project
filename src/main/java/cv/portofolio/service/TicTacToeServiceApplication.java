76b56e35ec0fc01863fb52cdf6d489f7e52e15f6
Stefan <belciugstefan@gmail.com>
2025-11-03 21:34:00 +0000

    Spring boot application

---
+++ src/main/java/cv/portofolio/service/TicTacToeServiceApplication.java
@@ -0,0 +1,13 @@
+package cv.portofolio.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cv/portofolio/service/TicTacToeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.portofolio.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
