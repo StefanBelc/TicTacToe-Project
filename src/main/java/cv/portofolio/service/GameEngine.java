888095ae58556653960cb6cf450fecf292467716
Stefan <belciugstefan@gmail.com>
2025-11-11 07:42:28 +0000

    methods: isGameOver(), initPlayers(), startGame() added to GameEngine

---
+++ src/main/java/cv/portofolio/service/GameEngine.java
@@ -0,0 +1,45 @@
+package cv.portofolio.service;
+
+import java.util.List;
+
+public class GameEngine {
+    private List<Player> players;
+    private int currentPlayerIndex;
+    GameState gameState = new GameState();
+
+
+    public void initPlayers() {
+        Player tic = new Player("Tic");
+        Player tac = new Player("Tac");
+        players.add(tic);
+        players.add(tac);
+    }
+
+
+    public Boolean isGameOver() {
+        if (gameState.hasTicWon()) {
+            return true;
+        } else if (gameState.hasTacWon()) {
+            return true;
+        } else if (gameState.isDraw()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    public String startGame() {
+        String result = "";
+        initPlayers();
+        while (!isGameOver()) {
+            players.get(0).makeDecision(gameState.availablePositions());
+            players.get(1).makeDecision(gameState.availablePositions());
+        }
+
+
+        return result;
+    }
+
+}
