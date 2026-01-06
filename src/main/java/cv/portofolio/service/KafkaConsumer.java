1fe1f3e1073a1337a7133c3743e1034226bb2e90
Stefan <belciugstefan@gmail.com>
2026-01-06 10:39:10 +0000

    PA-37 - Kafka + TicTacToe connectivity test

---
+++ src/main/java/cv/portofolio/service/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package cv.portofolio.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test-topic", groupId = "my-local-dev-group")
+    public void listen(String message) {
+        System.out.println("✅ Received " + message);
+    }
+
+}
